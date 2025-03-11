5d82ee2ad868493b08c10cb02a252142425ff481
Louis Roehrs <lroehrs@gmail.com>
2025-03-10 19:05:09 -0700

    Initial Commit

---
+++ HomeControlTests/HomeControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeControlTests.swift
+//  HomeControlTests
+//
+//  Created by Louis Roehrs on 3/10/25.
+//
+
+import Testing
+@testable import HomeControl
+
+struct HomeControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
