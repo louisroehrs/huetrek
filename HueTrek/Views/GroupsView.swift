acc273d73c8520d1c37249411bf66c213a7a9de4
Louis Roehrs <lroehrs@gmail.com>
2025-04-23 23:58:15 -0700

    Groups is now LCARS

--- HueTrek/Views/GroupsView.swift
+++ HueTrek/Views/GroupsView.swift
@@ -13,13 +13,12 @@ struct GroupsView: View {
     
     var body: some View {
         VStack {
-            
-            
             List {
                 ForEach(hueManager.groups) { group in
                     GroupRowView(group: group)
                         .listRowBackground(Color.black)
                         .background(Color.black)
+                        .padding(.top, 20)
                 }
             }
             .listStyle(.plain)
@@ -51,89 +50,148 @@ struct GroupRowView: View {
     var body: some View {
         
         VStack(spacing: 8) {
-            Rectangle()
-                .fill(Color.cyan)
-                .frame(height:2)
-                .padding(0)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(group.name)
-                        .font(Font.custom("Okuda", size: 30))
-                        .foregroundColor(.blue)
-                        .textCase(.uppercase)
-                    
-                    Text("\(group.lights.count) lights")
-                        .font(Font.custom("Okuda", size: 24))
-                        .foregroundColor(.gray)
-                }
-                
-                Spacer()
+            HStack(spacing: 6) {
+                TopLeftRoundedRectangle(radius: hueManager.ui.footerHeight)
+                    .fill(Color.cyan)
+                    .frame(maxWidth: .infinity, maxHeight: hueManager.ui.footerHeight)
+                    .overlay(alignment: .leading) {
+                        Text(group.name)
+                            .font(Font.custom("Okuda", size: hueManager.ui.footerLabelFontSize))
+                            .kerning(1.2)
+                            .foregroundColor(.cyan)
+                            .textCase(.uppercase)
+                            .padding(.leading, 5)
+                            .background(Color.black)
+                            .offset(x:40,y:0)
+                    }
                 
                 Button(action: {
                     hueManager.toggleGroup(group)
                     hueManager.playSound(sound: "light" + (group.action.on ? "Off" : "On"))
                 }) {
-                    Image(systemName: group.action.on ? "power.circle.fill" : "power.circle")
+            Image(systemName: group.action.on ? "power.circle.fill" : "power.circle")
                         .font(.title)
                         .foregroundColor(group.action.on ? .green : .red)
                 }
             }
-        
-            HStack {
-                Image(systemName: "sun.min")
-                    .foregroundColor(.gray)
-                GroupSliderView(sliderValue: Binding(
-                    get: {
-                        Double(group.action.bri)
-                    },
-                    set: { sliderValue in
-                        var myGroup = hueManager.groups.first(where: {$0.id == group.id} )
-                        var myGroupIndex = hueManager.groups.firstIndex(where: {$0.id == group.id} )
-                        myGroup!.action.bri = Int(sliderValue)
-                        hueManager.groups[myGroupIndex!] = myGroup!
+            .frame(maxWidth: .infinity, maxHeight: hueManager.ui.footerHeight)
+            
+            VStack(){
+                HStack(spacing:3) {
+
+                    Text("\(group.lights.count)")
+                        .font(Font.custom("Okuda", size: 36))
+                        .foregroundColor(.gray)
+                        .padding(.leading)
+                    Text((group.lights.count > 1) ? " lights " : " light ")
+                        .font(Font.custom("Okuda", size: 24))
+                        .textCase(.uppercase)
+                        .offset(x:0,y:3)
+                        .foregroundColor(.black)
+                        .background(Color.gray)
+                    Text("\(group.action.bri)")
+                        .font(Font.custom("Okuda", size: 36))
+                        .foregroundColor(.gray)
+                        .padding(0)
+                    Text(" b ")
+                        .font(Font.custom("Okuda", size: 24))
+                        .textCase(.uppercase)
+                        .offset(x:0,y:3)
+                        .foregroundColor(.black)
+                        .background(Color.gray)
+                    Text("\(group.action.hue)".replacing(",", with:""))
+                        .font(Font.custom("Okuda", size: 36))
+                        .foregroundColor(.gray)
+                        .padding(0)
+                    Text(" h ")
+                        .font(Font.custom("Okuda", size: 24))
+                        .textCase(.uppercase)
+                        .offset(x:0,y:3)
+                        .foregroundColor(.black)
+                        .background(Color.gray)
+                    Text("\(group.action.sat)")
+                        .font(Font.custom("Okuda", size: 36))
+                        .foregroundColor(.gray)
+                        .padding(0)
+                    
+                    Text(" s ")
+                        .font(Font.custom("Okuda", size: 24))
+                        .textCase(.uppercase)
+                        .offset(x:0,y:3)
+                        .foregroundColor(.black)
+                        .background(Color.gray)
+                    Spacer()
+                    Image(systemName: "paintpalette.fill")
+                        .imageScale(.large)
+                        .foregroundColor(Color(hue: Double(group.action.hue) / 65536.0, saturation: Double(group.action.sat) / 255.0, brightness: Double(group.action.bri) / 254.0))
+                        .frame(width:40, height:40)
+                        .background(Color.black)
+                        .clipped()
+                        .overlay
+                    {
+                        ColorPicker(
+                            "",
+                            selection: Binding(
+                                get: { selectedColor ?? hueManager.hueLightToSwiftColor(light: HueManager.Light(
+                                    id: group.id,
+                                    name: group.name,
+                                    state: HueManager.Light.State(
+                                        on: group.action.on,
+                                        bri: group.action.bri,
+                                        hue: group.action.hue,
+                                        sat: group.action.sat,
+                                        reachable: true
+                                    )
+                                ))
+                                },
+                                set: { newColor in
+                                    selectedColor = newColor
+                                    hueManager.updateGroupColor(group, color: newColor)
+                                }),
+                            supportsOpacity: false)
+                        .labelsHidden()
+                        .opacity(0.10)
+                        .frame(width:40, height: 40)
+                        .clipped()
                     }
+                }
+                .frame(maxWidth: .infinity, maxHeight:40)
+                .padding(.trailing,0)
+                
+                HStack {
+                    Image(systemName: "sun.min")
+                        .foregroundColor(.gray)
+                    GroupSliderView(sliderValue: Binding(
+                        get: {
+                            Double(group.action.bri)
+                        },
+                        set: { sliderValue in
+                            var myGroup = hueManager.groups.first(where: {$0.id == group.id} )
+                            var myGroupIndex = hueManager.groups.firstIndex(where: {$0.id == group.id} )
+                            myGroup!.action.bri = Int(sliderValue)
+                            hueManager.groups[myGroupIndex!] = myGroup!
+                        }
                     ),
                     group: group)
-                Image(systemName: "sun.max")
-                    .foregroundColor(.gray)
+                    Image(systemName: "sun.max")
+                        .foregroundColor(.gray)
+                }
+                .padding()
+                
+                
             }
-            
-            Image(systemName: "paintpalette.fill")
-                .imageScale(.large)
-                .foregroundColor(Color(hue: Double(group.action.hue) / 65536.0, saturation: Double(group.action.sat) / 255.0, brightness: Double(group.action.bri) / 254.0))
-                .frame(width:40, height:40)
-                .background(Color.black)
-                .clipped()
-                .overlay
-            {
-                ColorPicker(
-                    "",
-                    selection: Binding(
-                        get: { selectedColor ?? hueManager.hueLightToSwiftColor(light: HueManager.Light(
-                            id: group.id,
-                            name: group.name,
-                            state: HueManager.Light.State(
-                                on: group.action.on,
-                                bri: group.action.bri,
-                                hue: group.action.hue,
-                                sat: group.action.sat,
-                                reachable: true
-                            )
-                        ))
-                        },
-                        set: { newColor in
-                            selectedColor = newColor
-                            hueManager.updateGroupColor(group, color: newColor)
-                        }),
-                    supportsOpacity: false)
-                .labelsHidden()
-                .opacity(0.10)
-                .frame(width:40, height: 40)
-                .clipped()
+            .padding(.leading, 10)
+            .overlay(alignment: .leading) {
+                Rectangle()
+                    .fill(Color.cyan)
+                    .frame(width: 5)
             }
-            
+            BottomLeftRoundedRectangle(radius: 20)
+                .fill(.cyan)
+                .frame(height: 20)
+                
         }
-        .padding()
+        .padding(0)
     }
 }
 
